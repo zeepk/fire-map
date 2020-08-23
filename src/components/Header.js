238408f81de10362486e1776ea68e3c0bc6fdecd
zeepk <zeepkdb@gmail.com>
2020-08-22 17:19:02 -0700

    fontsize

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div style={{ padding: '3vh 0 0 5vw' }}>
-			<span style={{ fontSize: '2rem', color: 'red' }}>Wild</span>
-			<span style={{ fontSize: '2rem', color: 'orange' }}>Fires</span>
+		<div style={{ padding: '4vh 0 0 5vw' }}>
+			<span style={{ fontSize: 'calc(20px + 3vw)', color: 'red' }}>Wild</span>
+			<span style={{ fontSize: 'calc(20px + 3vw)', color: 'orange' }}>
+				Fires
+			</span>
 		</div>
 	);
 };
